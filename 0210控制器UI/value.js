e61f1f57cac57cf09bebe95cc30418c3867afff3
HsiangOOR <126255937+HsiangOOR@users.noreply.github.com>
2023-03-11 21:42:00 +0800

    UI design

---
+++ 0210控制器UI/value.js
@@ -0,0 +1,3 @@
+var data = {"AP":"webduino.io", "IP":"192.168.10.127", "MAC":"90:38:0C:6A:A8:BC", "Ver":"0.0.1 (Feb  1 2023:11:17:22)", "ssid":"", "passwd":"", "devId":"wdEaV", "devSSID":"wf8001", "devPasswd":"12345678", "openAp":"Yes", "broker":"", "port":0}
+
+    
